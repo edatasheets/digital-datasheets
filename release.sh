7cb6f81acfdf02fab51cb9eb4f6594046cbeb3f1
Rachel Nancollas <rachelsn@google.com>
2024-04-02 22:39:54 -0400

    renamed Examples to examples and updated release script

--- release.sh
+++ release.sh
@@ -6,9 +6,11 @@ DST=~/edatasheets.github.io
 rm -r "$DST/*"
 
 # TODO: copy README too
-cp -r "$SRC/part-spec" "$SRC/support-docs" "$SRC/LICENSE" "$DST"
+cp -r "$SRC/part-spec" "$SRC/support-docs" "$SRC/examples" "$SRC/LICENSE" "$DST"
 
 mv "$DST/support-docs/README.md" "$DST"
+rm -r "$DST/support-docs"
+
 
 # Replace the $id line with the official release github URI in every JSON file in part-spec.
 sed -i 's|"$id":.*\(/part-spec/.*\)$|"$id": "https://github.com/edatasheets/edatasheets.github.io/blob/main\1|' $(find "part-spec/" | grep '\.json$')
