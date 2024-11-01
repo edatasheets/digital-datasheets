797dc102bf04ed1334bc9202394cf63993dd74c4
Rachel Nancollas <rachelsn@google.com>
2024-11-01 09:55:15 -0700

    final changes to release script

--- release.sh
+++ release.sh
@@ -11,6 +11,9 @@ cp -r "$SRC/part-spec" "$SRC/support-docs" "$SRC/examples" "$SRC/LICENSE" "$DST"
 mv "$DST/support-docs/README.md" "$DST"
 rm -r "$DST/support-docs"
 
+# Rename the source information in the README with the official release github URI
+sed -i 's|https:/github.com/edatasheets/digital-datasheets/blob/main/part-spec/|https://github.com/edatasheets/edatasheets.github.io/blob/main/part-spec/|g' "$DST/README.md"
+sed -i 's|https://github.com/edatasheets/digital-datasheets/blob/main/part-spec/|https://github.com/edatasheets/edatasheets.github.io/blob/main/part-spec/|g' "$DST/README.md"
 
 # Replace the $id line with the official release github URI in every JSON file in part-spec.
 sed -i 's|"$id":.*\(/part-spec/.*\)$|"$id": "https://github.com/edatasheets/edatasheets.github.io/blob/main\1|' $(find "part-spec/" | grep '\.json$')
