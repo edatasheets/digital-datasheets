ebf845710ff4eaf1c07a273f19131e51d881e65b
rsnancollas <rachelsn@google.com>
2024-11-07 10:43:35 -0800

    Script update (#130)
    * updated readme text
    
    * change to release script
    
    * fixed issues with powerSequence ref

--- release.sh
+++ release.sh
@@ -6,8 +6,6 @@
 SRC=~/Documents/digital-datasheets
 DST=~/Documents/edatasheets.github.io
 
-rm -r "$DST/*"
-
 # TODO: copy README too
 cp -r "$SRC/part-spec" "$SRC/support-docs" "$SRC/examples" "$SRC/LICENSE" "$DST"
 
@@ -19,4 +17,5 @@ sed -i 's|https:/github.com/edatasheets/digital-datasheets/blob/main/part-spec/|
 sed -i 's|https://github.com/edatasheets/digital-datasheets/blob/main/part-spec/|https://github.com/edatasheets/edatasheets.github.io/blob/main/part-spec/|g' "$DST/README.md"
 
 # Replace the $id line with the official release github URI in every JSON file in part-spec.
+cd "$DST"
 sed -i 's|"$id":.*\(/part-spec/.*\)$|"$id": "https://github.com/edatasheets/edatasheets.github.io/blob/main\1|' $(find "part-spec/" | grep '\.json$')
